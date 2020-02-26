817a66fe40a99c71c7ec1dca7346fd544477056f
Lisa-Thuo <jebetthuo@gmail.com>
2020-02-26 09:41:07 +0300

    everything

--- scripts.js
+++ scripts.js
@@ -16,15 +16,40 @@ $(document).ready(function(){
     $(".list-hidden").toggle();
   });
 });
-$(document).ready(function(){
-  $(".gray").click(function(){
-    $(".blank-showing").toggle();
+$(document).ready(function () {
+  $(".gray").mouseover(function () {
+    $(".blank-showing").hover();
     $(".blank-hidden").toggle();
   });
 });
-
-
-
+$(".ontario").mouseover(function () {
+  $(".ontario-showing").hover();
+  $(".ontario-hidden").toggle();
+});
+$(".orange").mouseover(function () {
+  $(".orange-showing").hover();
+  $(".orange-hidden").toggle();
+});
+$(".hair").mouseover(function () {
+  $(".hair-showing").hover();
+  $(".hair-hidden").toggle();
+});
+$(".elena").mouseover(function () {
+  $(".elena-showing").hover();
+  $(".elena-hidden").toggle();
+});
+$(".button").mouseover(function () {
+  $(".button-showing").hover();
+  $(".button-hidden").toggle();
+});
+$(".burned").mouseover(function () {
+  $(".burned-showing").hover();
+  $(".burned-hidden").toggle();
+});
+$(".giraffe").mouseover(function () {
+  $(".giraffe-showing").hover();
+  $(".giraffe-hidden").toggle();
+});
 
 
 
